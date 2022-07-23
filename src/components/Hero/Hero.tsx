8716ae3d6a407dee271cd5b5e32df9bdabb73b18
Anton Metlitsky <ticket1201@gmail.com>
2022-07-23 22:09:23 +0300

    feat: implement Hero component with styles

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,12 +3,14 @@ import s from './Hero.module.scss';
 
 const Hero = () => {
     return (
-        <section>
-            <div className={`wrapper ${s.wrapper}`}>
+        <section className={s.hero}>
+            <div className={`wrapper ${s.wrapper}`} id={'main'}>
                 <div className={s.textWrapper}>
-                    <h2 className={s.sectionTitle}>Hey!</h2>
-                    <h2 className={s.sectionTitle}>I'm Anthony Metlitsky.</h2>
-                    <h2 className={s.sectionTitle}>Front-end Developer.</h2>
+                    <h1 className={s.sectionTitle}>
+                        <span className={s.titleItalic}>Hey!</span>
+                        <span>I'm Anthony Metlitsky</span>
+                        <span>Front-end Developer</span>
+                    </h1>
                 </div>
                 <div className={s.imageWrapper}>
                     <img src="" alt=""/>
