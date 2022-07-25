096cbf6c90e9ea36d932263f10ae3f5a00823066
Anton Metlitsky <ticket1201@gmail.com>
2022-07-25 14:44:31 +0300

    refactor: add props for skillCard

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -8,9 +8,9 @@ const Skills = () => {
             <div className={`wrapper ${s.wrapper}`}>
                 <h2 className={s.title}>Skills</h2>
                 <div className={`wrapper ${s.cardContainer}`}>
-                    <SkillCard/>
-                    <SkillCard/>
-                    <SkillCard/>
+                    <SkillCard skillTitle={'HTML'} skillDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'} imgUrl={''} imgAlt={'HTML image'}/>
+                    <SkillCard skillTitle={'CSS'} skillDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'} imgUrl={''} imgAlt={'CSS image'}/>
+                    <SkillCard skillTitle={'JS'} skillDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'} imgUrl={''} imgAlt={'JS image'}/>
                 </div>
             </div>
         </section>
