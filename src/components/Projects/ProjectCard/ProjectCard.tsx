b96e2739c51301d4a399fc3327a33b18f204827a
Anton Metlitsky <ticket1201@gmail.com>
2022-07-25 14:45:05 +0300

    refactor: add types and props for ProjectCard

--- src/components/Projects/ProjectCard/ProjectCard.tsx
+++ src/components/Projects/ProjectCard/ProjectCard.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import s from './../Projects.module.scss'
 
-const ProjectCard = () => {
+type ProjectPropsType = {
+    projectUrl:string
+    projectName:string
+    projectDescription:string
+}
+
+const ProjectCard = (props: ProjectPropsType) => {
+
     return (
         <div className={s.projectCard}>
             <div className={s.imageWrapper}>
                 <button className={s.hiddenBtn}>View project</button>
             </div>
             <div className={s.descriptionWrapper}>
-                <h3>Project name</h3>
-                <p className={s.projectDescription}>Short description</p>
+                <h3>{props.projectName}</h3>
+                <p className={s.projectDescription}>{props.projectDescription}</p>
             </div>
         </div>
     );
