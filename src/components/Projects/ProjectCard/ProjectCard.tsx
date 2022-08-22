437c3959ba067c2fd34263475c391b741ef9837f
Anton Metlitsky <ticket1201@gmail.com>
2022-08-22 21:04:57 +0300

    feat: add backgroundImage object with url of image from props, add require props

--- src/components/Projects/ProjectCard/ProjectCard.tsx
+++ src/components/Projects/ProjectCard/ProjectCard.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import s from './../Projects.module.scss'
 
 type ProjectPropsType = {
-    projectUrl:string
-    projectName:string
-    projectDescription:string
+    projectImage: string
+    projectUrl: string
+    projectName: string
+    projectDescription: string
 }
 
 const ProjectCard = (props: ProjectPropsType) => {
 
+    let backgroundImage = {
+        backgroundImage: `url(${props.projectImage})`
+    }
+
     return (
         <div className={s.projectCard}>
-            <div className={s.imageWrapper}>
+            <div className={s.imageWrapper} style={backgroundImage}>
                 <button className={s.hiddenBtn}>View project</button>
             </div>
             <div className={s.descriptionWrapper}>
