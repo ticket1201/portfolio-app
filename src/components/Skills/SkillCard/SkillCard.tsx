d1220d9f2fd843b13befcc7dfead4b8a3594069d
Anton Metlitsky <ticket1201@gmail.com>
2022-08-22 21:07:54 +0300

    feat: add fontawesome icon, require props

--- src/components/Skills/SkillCard/SkillCard.tsx
+++ src/components/Skills/SkillCard/SkillCard.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import s from './../Skills.module.scss'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {IconProp} from '@fortawesome/fontawesome-svg-core';
 
-type SkillCardType = {
-    imgUrl: string
-    imgAlt: string
+type SkillCardPropsType = {
+    iconName: IconProp
+    iconColor: string
     skillTitle: string
     skillDescription: string
 }
 
-const SkillCard = (props:SkillCardType) => {
+
+const SkillCard = (props:SkillCardPropsType) => {
     return (
         <div className={s.skillCard}>
-            <img src={props.imgUrl} alt={props.imgAlt}/>
+            <FontAwesomeIcon icon={props.iconName} color={props.iconColor}/>
             <h3 className={s.CardTitle}>{props.skillTitle}</h3>
             <p className={s.CardDescription}>{props.skillDescription}</p>
         </div>
