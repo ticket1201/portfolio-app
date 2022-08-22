2cc7a7d9d98a1d25d3c5a6258b47815f44a79577
Anton Metlitsky <ticket1201@gmail.com>
2022-08-22 21:06:48 +0300

    feat: add import images for projects, set require props for props

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import s from   './Projects.module.scss'
 import ProjectCard from './ProjectCard/ProjectCard';
+import todo from './../../assets/images/todolist.png'
+import social from './../../assets/images/socialNetwork.png'
 
 const Projects = () => {
     return (
@@ -8,8 +10,8 @@ const Projects = () => {
             <div className={`wrapper ${s.projectsWrapper}`} id={`projects`}>
                 <h2 className={s.projectTitle}>Projects</h2>
                 <div className={s.projectItemWrapper}>
-                    <ProjectCard projectName={'TodoList'} projectUrl={''} projectDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}/>
-                    <ProjectCard projectName={'Social Network'} projectUrl={''} projectDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}/>
+                    <ProjectCard projectName={'TodoList'} projectUrl={''} projectImage={todo} projectDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}/>
+                    <ProjectCard projectName={'Social Network'} projectImage={social} projectUrl={''} projectDescription={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}/>
                 </div>
             </div>
         </article>
