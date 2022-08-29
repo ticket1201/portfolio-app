1b4a3a03d6c72da4b9e1cd7f81f891499b4e7c87
Anton Metlitsky <ticket1201@gmail.com>
2022-08-29 14:34:46 +0300

    feat: implement Hero components with styles

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import s from './Hero.module.scss';
-import me from './../../assets/images/me.jpg'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faFacebookF, faGithub, faLinkedin, faTelegram} from '@fortawesome/free-brands-svg-icons';
+import {Fade} from 'react-awesome-reveal';
 
 const Hero = () => {
     return (
-        <section className={s.hero}>
-            <div className={`wrapper ${s.wrapper}`} id={'main'}>
-                <div className={s.textWrapper}>
-                    <h1 className={s.sectionTitle}>
-                        <span className={s.titleItalic}>Hey!</span>
-                        <span>I'm Anthony Metlitsky</span>
-                        <span>Front-end Developer</span>
-                    </h1>
+        <Fade className={s.fade}>
+            <section className={s.hero}>
+                <div className={`wrapper ${s.wrapper}`}>
+                    <div className={s.imageWrapper}>
+                        <div className={s.img}></div>
+                    </div>
+                    <div className={s.textWrapper}>
+                        <h1>Anton Metlitsky</h1>
+                        <p>Creative Frontend developer living in Minsk and happy to make your pages looks awesome.</p>
+                        <div className={s.iconsWrapper}>
+                            <a href="https://github.com/ticket1201">
+                                <FontAwesomeIcon icon={faGithub}/>
+                            </a>
+                            <a href="https://www.linkedin.com/in/anton-metlitsky-3568a113a" target={'_blank'}
+                               rel="noreferrer">
+                                <FontAwesomeIcon icon={faLinkedin}/>
+                            </a>
+                            <a href="https://www.facebook.com/profile.php?id=100001814025172" target={'_blank'}
+                               rel="noreferrer">
+                                <FontAwesomeIcon icon={faFacebookF}/>
+                            </a>
+                            <a href="https://t.me/followticket" target={'_blank'} rel="noreferrer">
+                                <FontAwesomeIcon icon={faTelegram}/>
+                            </a>
+                            <a href="mailto:ticket1201@gmail.com">
+                                <FontAwesomeIcon icon={faEnvelope}/>
+                            </a>
+                        </div>
+                    </div>
                 </div>
-                <div className={s.imageWrapper}>
-                    <img src={me} alt='Me'/>
-                </div>
-            </div>
-        </section>
+            </section>
+        </Fade>
     );
 };
 
