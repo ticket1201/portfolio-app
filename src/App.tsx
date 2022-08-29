402234381ce4e314ba7d3bfc91bbdebf3376353c
Anton Metlitsky <ticket1201@gmail.com>
2022-08-29 14:32:54 +0300

    feat: add routes to project

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,26 @@ import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
-import Skills from './components/Skills/Skills';
 import Projects from './components/Projects/Projects';
 import Contacts from './components/Contacts/Contacts';
-import Footer from './components/Footer/Footer';
-import ToTop from './components/ToTop/ToTop';
+import {Route, Routes} from 'react-router-dom';
+import About from './components/About/About';
+import Burger from './components/Burger/Burger';
 
 function App() {
-  return (
-    <main className="App">
-        <Header/>
-        <Hero/>
-        <Skills/>
-        <Projects/>
-        <Contacts/>
-        <Footer/>
-        <ToTop/>
-    </main>
-  );
+
+    return (
+        <main className="App">
+            <Header/>
+            <Burger/>
+            <Routes>
+                <Route path="/*" element={<Hero/>}/>
+                <Route path="contact" element={<Contacts/>}/>
+                <Route path="about" element={<About/>}/>
+                <Route path="portfolio" element={<Projects/>}/>
+            </Routes>
+        </main>
+    )
 }
 
 export default App;
