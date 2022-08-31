fdcfa7126f8d46442c0c37e42ca8b784ac63e3ab
Anton Metlitsky <ticket1201@gmail.com>
2022-08-31 15:24:43 +0300

    feat: rework skill component with styles

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import s from './Skills.module.scss'
-import SkillCard from './SkillCard/SkillCard';
-import {faCss3Alt, faHtml5, faSquareJs} from '@fortawesome/free-brands-svg-icons';
+import SkillCardSoft from './SkillCardSoft/SkillCardSoft';
+import SkillCardHard from './SkillCardHard/SkillCardHard';
+import Fact from './Fact/Fact';
 const Skills = () => {
     return (
         <section className={s.skills} id={`skills`}>
-            <div className={`wrapper ${s.wrapper}`}>
-                <div className={`wrapper ${s.cardContainer}`}>
-                    <SkillCard skillTitle={'HTML'} iconName={faHtml5} iconColor={'#e54c21'}/>
-                    <SkillCard skillTitle={'CSS'}  iconName={faCss3Alt} iconColor={'#264de4'}/>
-                    <SkillCard skillTitle={'JS'} iconName={faSquareJs} iconColor={'#f7e018'}/>
+            <div className={s.wrapper}>
+                <div className={`${s.cardContainer}`}>
+                    <h3>Hard Skills</h3>
+                    <div className={s.hardSkillWrapper}>
+                        <SkillCardHard skillTitle={'HTML5'}/>
+                        <SkillCardHard skillTitle={'JavaScript'}/>
+                        <SkillCardHard skillTitle={'Git'}/>
+                        <SkillCardHard skillTitle={'CSS3'}/>
+                        <SkillCardHard skillTitle={'Sass'}/>
+                        <SkillCardHard skillTitle={'FLUX'}/>
+                        <SkillCardHard skillTitle={'React'}/>
+                        <SkillCardHard skillTitle={'Redux'}/>
+                        <SkillCardHard skillTitle={'Hooks'}/>
+                        <SkillCardHard skillTitle={'Typescript'}/>
+                        <SkillCardHard skillTitle={'Axios'}/>
+                        <SkillCardHard skillTitle={'Thunk'}/>
+                        <SkillCardHard skillTitle={'Rest API'}/>
+                        <SkillCardHard skillTitle={'Heroku'}/>
+                    </div>
+                </div>
+                <div className={`${s.cardContainer}`}>
+                    <h3>Soft Skills</h3>
+                    <SkillCardSoft skillTitle={'English'} lvl={'B1'} percent={'70%'}/>
+                    <SkillCardSoft skillTitle={'Russian' } lvl={'Native'} percent={'100%'}/>
+                    <SkillCardSoft skillTitle={'Belarusian'} lvl={'Native'} percent={'100%'}/>
+                </div>
+            </div>
+            <div className={`${s.cardContainer}`}>
+                <h3>Fun Facts</h3>
+                <div className={s.factWrapper}>
+                    <Fact title={'3+'} descr={'Project Completed'}/>
+                    <Fact title={'5.4K+'} descr={'Lines of Code'}/>
+                    <Fact title={'14+'} descr={'Technologies learned'}/>
                 </div>
             </div>
         </section>
