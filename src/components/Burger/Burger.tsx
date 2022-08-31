f4aa0d15331b94914e62c3169a8f488f6b9e8719
Anton Metlitsky <ticket1201@gmail.com>
2022-08-31 15:40:31 +0300

    feat: rework burger component to use state from app for open header

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -3,9 +3,14 @@ import s from './Burger.module.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faAlignJustify} from '@fortawesome/free-solid-svg-icons';
 
-const Burger = () => {
+type BurgerPropsType ={
+
+    callback: (isOpen:boolean) => void
+}
+
+const Burger = ({callback}: BurgerPropsType) => {
     return (
-        <div className={s.burger}>
+        <div className={s.burger} onClick={() => callback(false)}>
             <FontAwesomeIcon icon={faAlignJustify}/>
         </div>
     );
