5be6c2306df9d7762c209e0f230bcb1f4ab3bfbe
Anton Metlitsky <ticket1201@gmail.com>
2022-08-31 15:36:47 +0300

    feat: rework Header component with styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,12 @@ import {faBriefcase, faEnvelope, faHouseChimney, faUser} from '@fortawesome/free
 import {NavLink} from 'react-router-dom';
 import {Fade} from 'react-awesome-reveal';
 
-const Header = () => {
+type HeaderPropsType = {
+    isClosed: boolean
+    callback: (isOpen: boolean) => void
+}
+
+const Header = ({isClosed, callback}: HeaderPropsType) => {
     const active = {
         color: `black`,
         letterSpacing: `2px`
@@ -13,32 +18,37 @@ const Header = () => {
 
     let date = new Date()
 
+
+    let styleFade = isClosed && window.innerWidth < 1200 ? s.hidden : '';
+
     return (
-        <Fade>
-            <header>
-                <h2>PORTFOLIO</h2>
-                <nav className={s.nav}>
-                    <NavLink to={'/home'} className={s.navItem}
-                             style={({isActive}) =>
-                                 isActive ? active : {}}>
-                        <FontAwesomeIcon icon={faHouseChimney}/>
-                        Home
-                    </NavLink>
-                    <NavLink to={'/about'} className={s.navItem}>
-                        <FontAwesomeIcon icon={faUser}/>
-                        About
-                    </NavLink>
-                    <NavLink to={'/portfolio'} className={s.navItem}>
-                        <FontAwesomeIcon icon={faBriefcase}/>
-                        Portfolio
-                    </NavLink>
-                    <NavLink to={'/contact'} className={s.navItem}>
-                        <FontAwesomeIcon icon={faEnvelope}/>
-                        Contact
-                    </NavLink>
-                </nav>
-                <p>© {date.getFullYear()}. All Rights Reserved</p>
-            </header>
+        <Fade className={`${s.fade} ${styleFade}`}>
+            <div className={`${s.click}`} onClick={() => callback(true)}>
+                <header>
+                    <h2>PORTFOLIO</h2>
+                    <nav className={s.nav}>
+                        <NavLink onClick={() => callback(true)} to={'/home'} className={s.navItem}
+                                 style={({isActive}) =>
+                                     isActive ? active : {}}>
+                            <FontAwesomeIcon icon={faHouseChimney}/>
+                            Home
+                        </NavLink>
+                        <NavLink onClick={() => callback(true)} to={'/about'} className={s.navItem}>
+                            <FontAwesomeIcon icon={faUser}/>
+                            About
+                        </NavLink>
+                        <NavLink onClick={() => callback(true)} to={'/portfolio'} className={s.navItem}>
+                            <FontAwesomeIcon icon={faBriefcase}/>
+                            Portfolio
+                        </NavLink>
+                        <NavLink onClick={() => callback(true)} to={'/contact'} className={s.navItem}>
+                            <FontAwesomeIcon icon={faEnvelope}/>
+                            Contact
+                        </NavLink>
+                    </nav>
+                    <p>© {date.getFullYear()}. All Rights Reserved</p>
+                </header>
+            </div>
         </Fade>
     );
 };
