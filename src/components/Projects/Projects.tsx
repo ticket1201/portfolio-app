3e061a37e9b9e8e25d8cbe4b82b9762f5d612a42
Anton Metlitsky <ticket1201@gmail.com>
2022-10-13 23:54:43 +0400

    feat: add new project

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import s from   './Projects.module.scss'
+import s from './Projects.module.scss'
 import ProjectCard from './ProjectCard/ProjectCard';
 import todo from './../../assets/images/todolist.png'
 import social from './../../assets/images/socialNetwork.png'
+import cards from './../../assets/images/cards.jpg'
 import {Fade} from 'react-awesome-reveal';
 
 const Projects = () => {
@@ -12,8 +13,18 @@ const Projects = () => {
                 <h2 className={s.projectTitle}>Projects</h2>
                 <Fade className={s.fade}>
                     <div className={s.projectItemWrapper}>
-                        <ProjectCard projectName={'TodoList'} projectUrl={'https://ticket1201.github.io/it-incubator-todolist-ts-01/'} projectImage={todo} projectDescription={'Todolist, simple way to organise everything'}/>
-                        <ProjectCard projectName={'Social Network'} projectImage={social} projectUrl={'https://ticket1201.github.io/samurai-way/'} projectDescription={'Online platform to connect people'}/>
+                        <ProjectCard projectName={'Cards Quiz'} projectImage={cards}
+                                     projectUrl={'https://ticket1201.github.io/cards_quiz/'}
+                                     projectDescription={'Cards quiz application to learn new things'}/>
+
+                        <ProjectCard projectName={'TodoList'}
+                                     projectUrl={'https://ticket1201.github.io/it-incubator-todolist-ts-01/'}
+                                     projectImage={todo}
+                                     projectDescription={'Todolist, simple way to organise everything'}/>
+
+                        <ProjectCard projectName={'Social Network'} projectImage={social}
+                                     projectUrl={'https://ticket1201.github.io/samurai-way/'}
+                                     projectDescription={'Online platform to connect people'}/>
                     </div>
                 </Fade>
             </div>
